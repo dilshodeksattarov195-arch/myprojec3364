de8a392c5d36ece72b66765998538b96720fbe69
DILSHODBEK <dilshodsattar26@gmail.com>
2026-06-01 00:13:54 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class smsDtringifyWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = smsDtringifyWorker()
+    print("Worker engine initialized.")
